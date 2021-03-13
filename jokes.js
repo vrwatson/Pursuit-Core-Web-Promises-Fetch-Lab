4a8672a5efac676681f51286fc68d0a925d14816
vrwatson <vanessawatson@pursuit.org>
2021-03-13 18:09:07 -0500

    pushing again to make sure it's working

--- jokes.js
+++ jokes.js
@@ -1,30 +1,30 @@
-const ul = document.querySelector("#joke-list")
-const punchLine = []
+const ul = document.querySelector("#joke-list");
+const punchLine = [];
 
 const createJokes = () => {
-    fetch("https://official-joke-api.appspot.com/random_ten").then(res => {
-        if(!res.ok) {
-            throw Error("Not today Satan!")
-        } 
-        return res.json();
-    }).then(res => {
-        res.forEach(joke => {
-            const li = document.createElement("li");
-            punchLine.push(joke.punchline);
-            li.textContent = joke.setup;
-            ul.appendChild(li)
-        });
-        // debugger 
-        // console.log(res)
+  fetch("https://official-joke-api.appspot.com/random_ten")
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Not today Satan!");
+      }
+      return res.json();
     })
-    .catch(err => {
-        console.log(err);
+    .then((res) => {
+      res.forEach((joke) => {
+        const li = document.createElement("li");
+        punchLine.push(joke.punchline);
+        li.textContent = joke.setup;
+        ul.appendChild(li);
+      });
     })
-    console.log(punchLine)
-}
+    .catch((err) => {
+      console.log(err);
+    });
+  console.log(punchLine);
+};
 createJokes();
 
 ul.addEventListener("click", (e) => {
-    const li = e.target;
-    // fetch
-})
+  const li = e.target;
+  // fetch
+});
